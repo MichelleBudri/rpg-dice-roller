8d4e7654be3b2a6d55b08d70da843a9e4f90aa40
Michelle Budri Bognar <michellebudri@yahoo.com>
2022-01-23 02:45:35 -0300

    #03 - Calculando resultados
    Calculando e exibindo o resultado do dado girado

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+/* tooltip */
+$('[data-toggle="tooltip"]').tooltip({
+    trigger : 'hover'
+})  
+
+/* Random Numbers */
+function getRandom(min, max) {
+    type = (max - 1);
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    calc = Math.floor(Math.random() * (max - min)) + min;
+    
+    document.getElementById("diceType").innerHTML = "1D"+type;
+    document.getElementById("result").innerHTML = calc;
+    
+    $('.description').removeClass('display-none');
+}
